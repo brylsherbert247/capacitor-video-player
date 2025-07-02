9052bb34cd6876f90d46c48d57f030913c9ff63f
Bryl Magdadaro <bryl@tvstartup.com>
2025-07-02 08:54:16 +0800

    "Include dist folder for GitHub installs"

---
+++ dist/index.d.ts
@@ -0,0 +1,5 @@
+import type { CapacitorVideoPlayerPlugin } from './definitions';
+declare const CapacitorVideoPlayer: CapacitorVideoPlayerPlugin;
+export * from './definitions';
+export { CapacitorVideoPlayer };
+//# sourceMappingURL=index.d.ts.map
---
+++ dist/web-utils/video-types.d.ts
@@ -0,0 +1,5 @@
+export type videoExtension = 'mp4' | 'webm' | 'cmaf' | 'cmfv' | 'm3u8';
+export type videoMimeType = 'video/mp4' | 'application/x-mpegURL';
+export declare const possibleQueryParameterExtensions: string[];
+export declare const videoTypes: Record<videoExtension, videoMimeType>;
+//# sourceMappingURL=video-types.d.ts.map
